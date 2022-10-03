16fdaabf4469657fc29d6a953d4da5d58258f56b
AvaP3 <84456250+avapierini@users.noreply.github.com>
2022-10-03 13:02:24 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+let age=25;
+let year=2019;
+
+//log things to console
+//console.log(age,year);
+
+
+/* age=30;
+console,log(age);
+
+const points=100
+console.log(points); */
+
+var score = 75;
+console.log(score);
+
+//strings
+console.log('hello, world');
+
+let email= 'mario@thenetninja.co.uk';
+console.log(email);
+//string concatebation
+ let FirstName= 'Brandon';
+let lastName='Sanderson';
+
+let FullName = FirstName +''+ lastName;
+console.log(fullName);
+
+//getting characters
+console.log(FullName)
+//getting characters
+console.log(FullName[2]);
+
+//string methods
+console.log(fullName.toUppercase());
+let result=FullName.toUpperCase());
+console.log(result, fullName)
+
+//console.log(result)
+let age=25;
+//console.log(age==25);
+//console.log(age==30);
+//console.log(age!=30);
+//console.log(age > =20);
+//console.log(age < 20);
+//console.log(age < = 25);
+//console.log(age> =25);
+
+let name='Shaun'
+console.log(name=='Shaun');
+console.log(name=='Shaun');
+console.log(name>'crystal');
+console.log(name>'Shaun');
+console.log(name>'crystal');
+
+// type conversation
+let score='100'
+//score = number(score);
+// console.log(score + 1);
+// console.log(typeof score);
+
+//let result = Number('hello');
+//let result =string(50);
+//let result= Boolean(0);
+let result= Boolean('0');
+
+console.log(result,typof result)
