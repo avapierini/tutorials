8248a632e37dbb1c9ad18c7ed681694f844f4126
AvaP3 <84456250+avapierini@users.noreply.github.com>
2022-10-03 13:28:28 +0000

    I added some of the 3rd tutorial and finsihed the 2nd

--- script (2).js
+++ script (2).js
@@ -1,6 +1,20 @@
 // for loops
 
-for(let i = 0); i < 5;i++){
-console.log('in loop:',i)
+//for(let i = 0); i < 5;i++){
+//console.log('in loop:',i)
   
+}
+//console.log('loop finished');
+
+const names=[ 'Shaun','Mario','Luigi'];
+
+for(let i = 0;i < names length;i++){
+//console.log(names[i]);
+  //let html= '<div>${names[i]</div>';
+  console.log(html);
+}
+//while loops
+
+for(let i=0; i < 5; i++){
+  console.log('in loop:',i);
 }
