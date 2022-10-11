30adf2925e408cee53e37c9941304edf0861b613
AvaP3 <84456250+avapierini@users.noreply.github.com>
2022-10-11 14:51:31 +0000

    finsihed

--- sandbox.js
+++ sandbox.js
@@ -124,7 +124,113 @@ let user=false;
 if(user)=false;
 
 if(!user){
-  console.log
+  console.log('You must be logged in to comtinue')
   
 }
 console.log(true);
+
+// break and continue
+
+const scores=[50,25,0,30,100,20,10]
+
+for(let i=0;i< scores.length; i++){
+
+  if(scores[i]===0{
+    continue
+  }
+  console.log('Your score:',scores[i]);
+
+  if(scores)[i]===100){
+    console.log('congrats,you got the top score!');
+    break;
+  }
+}
+
+//switch statements
+
+const grade='D';
+
+switch (grade){
+  case 'A';
+    console.log('You got an A!')
+
+      case 'B';
+    console.log('You got an B!')
+
+      case 'C';
+    console.log('You got an C!')
+
+      case 'D';
+    console.log('You got an D!')
+
+    case 'E';
+    console.log('You got an E!')
+
+  default:
+    console.log('not a valid grade')
+}
+
+//variables & block scope
+
+const age=30;
+
+if(true){
+const age= 40;
+  const name='Shaun';
+
+  console.log('inside 1st code block:', age, name);
+  
+
+  if(true){
+const age= 50;
+
+  console.log('inside 2nd code block:', age, name);
+}
+    console.log('outside code block:', age, name);
+}
+
+
+
+//function declaration
+const speak =function(){
+  Console.log('good day!')
+
+  //Greet();
+  //Greet();
+ // Greet();
+
+  speak();
+  speak();
+  speak();
+
+  //function declaration
+
+function greet(){
+console.log('hello there ');
+}
+
+//function expression 
+const speak= function(){
+
+console.log('good day!')
+};
+
+// returning values 
+
+  //const speak = function(name ='luigi', time='night'){
+//  console.log('good ${time} ${name} ');
+//};
+
+  const CalcArea= function(radius){
+    return 3.14* radius**2
+
+    const area= CalcArea(5);
+    console.log(area);
+
+    const caclVol= function(area){
+      
+    };
+
+      <h2> This is a new h2</h2>
+    }
+content.innerHTML='<h2> THIS IS A NEW H2</h2>;'
